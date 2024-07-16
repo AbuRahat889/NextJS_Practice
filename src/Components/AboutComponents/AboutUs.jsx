e0716391356fde173f0e0cbec482836e5a485dc6
AbuRahat889 <aburahatshaum889@gmail.com>
2024-07-16 22:41:29 +0600

    add route

---
+++ src/Components/AboutComponents/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <h1>call : 016465465</h1>
+            <h1>call : 016465465</h1>
+            <h1>call : 016465465</h1>
+            <h1>call : 016465465</h1>
+            <h1>call : 016465465</h1>
+        </div>
+    );
+};
+
+export default AboutUs;
---
+++ src/app/about/nextabout/page.jsx
@@ -0,0 +1,13 @@
+import AboutUs from "@/Components/AboutComponents/AboutUs";
+import React from "react";
+
+const NextAbout = () => {
+  return (
+    <div>
+      <h1>thisi s next abot page</h1>
+      <AboutUs></AboutUs>
+    </div>
+  );
+};
+
+export default NextAbout;
